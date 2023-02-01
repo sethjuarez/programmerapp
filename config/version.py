b60de661a8f59532e0038d4a00d39c088a7636e6
sethjuarez <me@sethjuarez.com>
2023-02-01 11:33:52 -0800

    augmented version logic

--- config/version.py
+++ config/version.py
@@ -1,13 +1,16 @@
 import argparse
 from datetime import datetime
 
-def main(version, tag=None):
-    datev = f'{datetime.now():%Y%m%d}'
-    dayv = 0
-    if datev in version:
-        dayv = int(version.split('.')[-1]) + 1
+def main(version, number, tag=None):
+    if number:
+        print(int(version.split('.')[-1]) + 1)
+    else:
+        datev = f'{datetime.now():%Y%m%d}'
+        dayv = 0
+        if datev in version:
+            dayv = int(version.split('.')[-1]) + 1
 
-    print(f'{tag + "." if tag else ""}{datev}.{dayv}')
+        print(f'{tag + "." if tag else ""}{datev}.{dayv}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -16,5 +19,6 @@ if __name__ == '__main__':
                     epilog = 'One version to rule them all, one version to find them, one version to bring them all and in the darkness bind them')
     parser.add_argument('-v', '--version', type=str, help='latest version', required=True)
     parser.add_argument('-t', '--tag', type=str, help='tag to prepend')
+    parser.add_argument('-n', '--number', action='store_true')
     args = parser.parse_args()
-    main(args.version, args.tag)
+    main(args.version, args.number, args.tag)
